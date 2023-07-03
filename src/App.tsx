f6ed17c5ed68a6485ad57109aa729b31dadbe135
Bharghav S <srikhakollu.bharghav@gmail.com>
2023-07-02 17:56:50 -0700

    Task-2 Changes

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  /* Bharghav - 07/02/2023 - add - To display graph results */
+  showGraph: boolean,
+  /* Bharghav - 07/02/2023 - end - To display graph results */
 }
 
 /**
@@ -22,6 +25,9 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+  /* Bharghav - 07/02/2023 - add - set to false, so that the graph displays only when user clicks the button */
+      showGraph: false,
+  /* Bharghav - 07/02/2023 - end - set to false, so that the graph displays only when user clicks the button */
     };
   }
 
@@ -29,19 +35,52 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+     /* Bharghav - 07/02/2023 - del - change the condition as below */
+     /* return (<Graph data={this.state.data}/>) */
+     /* Bharghav - 07/02/2023 - end - change the condition as below */
+
+     /* Bharghav - 07/02/2023 - add - to show graph only when showGraph is true */
+     if (this.state.showGraph) {
+        return (<Graph data={this.state.data}/>)
+     }
+     /* Bharghav - 07/02/2023 - end - - to show graph only when showGraph is true */
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
+
+/* Bharghav - 07/02/2023 - del - change the condition as below */
+  /*
   getDataFromServer() {
     DataStreamer.getData((serverResponds: ServerRespond[]) => {
       // Update the state by creating a new array of data that consists of
       // Previous data in the state and the new data from server
       this.setState({ data: [...this.state.data, ...serverResponds] });
     });
+  } */
+/* Bharghav - 07/02/2023 - del - change the condition as below */
+
+/* Bharghav - 07/02/2023 - add - change the condition as below */
+  /*
+  for every 100 milliseconds of time, the graph with data will be displayed for 500 iterations.
+  */
+  getDataFromServer() {
+    let count = 0;
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        this.setState({
+          data: serverResponds,
+          showGraph: true,
+        });
+      });
+      count++;
+      if (count > 500) {
+        clearInterval(interval);
+      }
+    }, 100);
   }
+/* Bharghav - 07/02/2023 - end - change the condition as below */
 
   /**
    * Render the App react component
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,7 +14,10 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+/* Bharghav - 07/02/2023 - mod - to make PerspectiveViewerElement behave as HTMLElement */
+/* interface PerspectiveViewerElement { */
+   interface PerspectiveViewerElement extends HTMLElement {
+/* Bharghav - 07/02/2023 - end - to make PerspectiveViewerElement behave as HTMLElement */
   load: (table: Table) => void,
 }
 
@@ -32,7 +35,22 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    /* Bharghav - 07/02/2023 - mod - assign directly to the result of document.getElementsByTagName */
+    /* const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement; */
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    /* Bharghav - 07/02/2023 - end - assign directly to the result of document.getElementsByTagName */
+
+    /* Bharghav - 07/02/2023 - add - added attributes to the element */
+    elem.setAttribute('view', 'y_line');
+    elem.setAttribute('column-pivots', '["stock"]');
+    elem.setAttribute('row-pivots', '["timestamp"]');
+    elem.setAttribute('columns', '["top_ask_price"]');
+    elem.setAttribute('aggregates', `
+      {"stock":"distinct count",
+      "top_ask_price": "avg",
+      "top_bid_price": "avg",
+      "timestamp": "distinct count"}`);
+    /* Bharghav - 07/02/2023 - end - added attributes to the element */
 
     const schema = {
       stock: 'string',
